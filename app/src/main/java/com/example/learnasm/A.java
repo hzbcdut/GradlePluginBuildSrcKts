1922c25f6aaada58b1ce68317ef7481d086582f0
huzhengbiao <hzhengbiao@126.com>
2022-10-16 11:28:53 +0800

    字节码插桩 插入 字节码指令
    java代码
    long startTime = System.currentTimeMillis();
    long endTime = System.currentTimeMillis();
    System.out.println("execute:"+(endTime-startTime)+"ms.");
    
    对应的字节码
    
    // access flags 0x1
      public hello()V
       L0
        LINENUMBER 12 L0
        INVOKESTATIC java/lang/System.currentTimeMillis ()J
        LSTORE 1
       L1
        LINENUMBER 13 L1
        INVOKESTATIC java/lang/System.currentTimeMillis ()J
        LSTORE 3
       L2
        LINENUMBER 14 L2
        GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
        NEW java/lang/StringBuilder
        DUP
        INVOKESPECIAL java/lang/StringBuilder.<init> ()V
        LDC "execute:"
        INVOKEVIRTUAL java/lang/StringBuilder.append (Ljava/lang/String;)Ljava/lang/StringBuilder;
        LLOAD 3
        LLOAD 1
        LSUB
        INVOKEVIRTUAL java/lang/StringBuilder.append (J)Ljava/lang/StringBuilder;
        LDC "ms."
        INVOKEVIRTUAL java/lang/StringBuilder.append (Ljava/lang/String;)Ljava/lang/StringBuilder;
        INVOKEVIRTUAL java/lang/StringBuilder.toString ()Ljava/lang/String;
        INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
       L3
        LINENUMBER 15 L3
        RETURN
       L4
        LOCALVARIABLE this Lcom/example/learnasm/A; L0 L4 0
        LOCALVARIABLE startTime J L1 L4 1
        LOCALVARIABLE endTime J L2 L4 3
        MAXSTACK = 6
        MAXLOCALS = 5

--- app/src/main/java/com/example/learnasm/A.java
+++ app/src/main/java/com/example/learnasm/A.java
@@ -6,7 +6,7 @@ public class A {
 
     public void hello() {
         // 这个函数里面什么都没写，字节码插桩插入语句
-//        Log.i("debug_asm", Log.getStackTraceString(new Throwable()));
+        // 先不用asm生成字节码指令，直接在代码中写看字节码指令是什么
 //        long startTime = System.currentTimeMillis();
 //        long endTime = System.currentTimeMillis();
 //        System.out.println("execute:"+(endTime-startTime)+"ms.");
