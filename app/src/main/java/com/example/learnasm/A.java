84165e3f8a5dbfba0fd3ac62f8888f63089b6790
huzhengbiao <hzhengbiao@126.com>
2022-10-16 11:33:29 +0800

    获取当前执行方法的名称

--- app/src/main/java/com/example/learnasm/A.java
+++ app/src/main/java/com/example/learnasm/A.java
@@ -1,10 +1,11 @@
 package com.example.learnasm;
 
-import android.util.Log;
 
 public class A {
 
     public void hello() {
+        String method = Thread.currentThread() .getStackTrace()[2].getMethodName();
+        System.out.println("当前执行方法的方法名 " + method);
         // 这个函数里面什么都没写，字节码插桩插入语句
         // 先不用asm生成字节码指令，直接在代码中写看字节码指令是什么
 //        long startTime = System.currentTimeMillis();
